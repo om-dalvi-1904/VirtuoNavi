ce6c5d75389a78f5533c89f17d8ec0543e17b52b
HarshalP05 <harshalmpatil2005@gmail.com>
2024-09-15 22:29:29 +0530

    integrate auth and imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { auth } from './firebaseConfig';
+import Home from './Home';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import Admin from './components/Admin/Admin'; // Import your Admin component
 import FrontPage from './components/FrontPage/FrontPage';
 
+
 function App() {
+  const [userDetails, setUserDetails] = useState(null); // State to store user details
+  const [userDocId, setUserDocId] = useState(null); // State to store user document ID
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // Track user authentication
+
+  // Check user authentication status
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsAuthenticated(true);
+        // You can fetch user details from Firebase and set it here
+        // setUserDetails({ name: user.displayName, email: user.email });
+      } else {
+        setIsAuthenticated(false);
+        setUserDetails(null);
+        setUserDocId(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="App">
       <FrontPage />
