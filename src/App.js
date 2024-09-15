f584adc6ea44a984dfda7e8697f1e5f3f0f75634
HarshalP05 <harshalmpatil2005@gmail.com>
2024-09-15 22:27:00 +0530

    Add routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ function App() {
   return (
     <div className="App">
       <FrontPage />
+      <Router>
+      <Routes>
+        {/* Redirect user to home if authenticated, otherwise show login */}
+        <Route path="/login" element={!isAuthenticated ? <Login setUserDetails={setUserDetails} setUserDocId={setUserDocId} /> : <Navigate to="/home" />} />
+
+        {/* Redirect to signup if user wants to sign up */}
+        <Route path="/Signup" element={<Signup />} />
+
+        {/* Home route, accessible only if user is authenticated */}
+        <Route path="/home" element={isAuthenticated ? <Home userDetails={userDetails} userDocId={userDocId} /> : <Navigate to="/login" />} />
+
+        {/* Admin route, accessible only if user is authenticated */}
+        <Route path="/admin" element={isAuthenticated ? <Admin userDetails={userDetails} userDocId={userDocId} /> : <Navigate to="/login" />} />
+      </Routes>
+    </Router>
     </div>
+    
   );
 }
 
